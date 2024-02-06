760c5201102ae7b36c44d8bc2aad5c3a68ee3153
Marco Antonio Caldeira <caldeiran391@gmail.com>
2024-02-05 23:59:57 -0300

    add tailwind and components

--- src/app.tsx
+++ src/app.tsx
@@ -15,15 +15,7 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
         <div className="grid grid-cols-3 gap-6  auto-rows-[250px]">
-          <div className="rounded-md bg-slate-700 p-5 space-y-3">
-              <span className="text-sm font-medium text-state-200">
-                Adicionar nota
-              </span>
-              <p className="text-sm leading-6 text-slate-400">
-                Grave uma nota em audio que sera convertida para texto automaticamente.
-              </p>
-          </div>
-
+          <NoteCard />
           <NoteCard />
           <NoteCard />
           <NoteCard />
